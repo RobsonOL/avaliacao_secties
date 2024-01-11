4b37563db6fadcd29cfaa6b6333a9ff6bba1844e
robson.o.l@hotmail.com <robson.o.l@hotmail.com>
2024-01-11 19:50:21 -0300

    adicionar tese

--- R/1.importar_dados.R
+++ R/1.importar_dados.R
@@ -4,14 +4,14 @@ needs::needs(tidyverse, vroom)
 
 #### DADOS -------------
 
-# Quantidade de bolsas por programa de pós-graduação ---------------------------
+# Quantidade de bolsas por programa de Pós-Graduação ---------------------------
 quantidade_bolsas <- list.files(path = "dados/brutos/concessao/", pattern = "*.zip", full.names = TRUE) |> 
   map_df(read_csv, locale = locale(encoding = 'latin5'))
 
 quantidade_bolsas |> write_rds("dados/brutos/quantidade_bolsas.rds")
 
 
-# Informações dos discentes de pós-graduação -----------------------------------
+# InformaÃ§Ãµes dos discentes de Pós-Graduação -----------------------------------
 # https://dadosabertos.capes.gov.br/dataset/discentes-da-pos-graduacao-stricto-sensu-do-brasil-2017-a-2019
 
 discentes <- list.files(path = "dados/brutos/discentes/", pattern = "*.zip", full.names = TRUE) |>  
@@ -20,7 +20,7 @@ discentes <- list.files(path = "dados/brutos/discentes/", pattern = "*.zip", ful
 discentes |> write_rds("dados/brutos/discentes.rds")
 
 
-# Produção de artigos em periódicos --------------------------------------------
+# ProduÃ§Ã£o de artigos em periÃ³dicos --------------------------------------------
 # https://dadosabertos.capes.gov.br/dataset/2017-a-2020-producao-intelectual-de-pos-graduacao-stricto-sensu-no-brasil
 producao_artigos_periodicos <- list.files(path = "dados/brutos/producao/",
                                           pattern = "artpe",
@@ -32,18 +32,68 @@ producao_artigos_periodicos <- list.files(path = "dados/brutos/producao/",
 producao_artigos_periodicos |> write_rds("dados/brutos/producao_artigos_periodicos.rds")
 
 
-# Teses e dissertações de discentes de pós-graduação ---------------------------
+# Teses e dissertaÃ§Ãµes de discentes de Pós-Graduação ---------------------------
 # https://dadosabertos.capes.gov.br/dataset/2017-2020-catalogo-de-teses-e-dissertacoes-da-capes
-teses_dissertacoes <- list.files(path = "dados/brutos/teses_dissertacoes/",
-                                 pattern = "*.zip",
+teses_dissertacoes_2013_2020 <- list.files(path = "dados/bruto/teses_dissertacoes/",
+                                 pattern = "br-capes",
                                  full.names = TRUE) |>
   map_df(read_csv2,
          locale = locale(encoding = 'latin5'),
          col_types = cols(.default = "c"))
 
-teses_dissertacoes |> write_rds("dados/brutos/teses_dissertacoes.rds")
+teses_dissertacoes_2010_2012 <- list.files(path = "dados/bruto/teses_dissertacoes/",
+                                           pattern = "dados_",
+                                           full.names = TRUE) |>
+  map_df(read_csv2,
+         locale = locale(encoding = 'latin5'),
+         col_types = cols(.default = "c"))
 
-# Bolsas de programas de pós-graduação -----------------------------------------
+
+teses_dissertacoes_2010_2012 <- teses_dissertacoes_2010_2012 |> 
+  rename(AN_BASE = AnoBase,
+         CD_PROGRAMA = CodigoPrograma,
+         NM_REGIAO = Regiao,
+         SG_UF_IES = Uf,
+         SG_ENTIDADE_ENSINO = SiglaIes,
+         NM_ENTIDADE_ENSINO = NomeIes,
+         NM_PROGRAMA = NomePrograma,
+         CD_GRANDE_AREA_CONHECIMENTO = GrandeAreaCodigo,
+         NM_GRANDE_AREA_CONHECIMENTO = GrandeAreaDescricao,
+         CD_AREA_CONHECIMENTO = AreaConhecimentoCodigo,
+         NM_AREA_CONHECIMENTO = AreaConhecimento,
+         NM_AREA_AVALIACAO = AreaAvaliacao,
+         NM_DISCENTE = Autor,
+         NM_PRODUCAO = TituloTese,
+         NM_GRAU_ACADEMICO = Nivel,
+         DT_TITULACAO = DataDefesa, # dúvida?
+         DS_PALAVRA_CHAVE = PalavrasChave,
+         NR_VOLUME = Volume,
+         NR_PAGINAS = NumeroPaginas,
+         DS_BIBLIOTECA_DEPOSITARIA = BibliotecaDepositaria,
+         NM_IDIOMA = Idioma,
+         DS_RESUMO = ResumoTese,
+         NM_LINHA_PESQUISA = LinhaPesquisa,
+         DS_URL_TEXTO_COMPLETO = URLTextoCompleto,
+         NM_ORIENTADOR = Orientador_1
+         ) |> 
+  select(-starts_with("Documento"), -starts_with("Orientador"),
+         -starts_with("CoOrientador"), -`%PDF-1.4`) |> 
+  mutate(DT_TITULACAO = as.Date(DT_TITULACAO, format = "%d/%m/%Y"))
+
+
+teses_dissertacoes_2013_2020 <- teses_dissertacoes_2013_2020 |> 
+  mutate(DT_TITULACAO = case_when(
+    AN_BASE <= 2016 ~ as.Date(DT_TITULACAO, format = "%d/%m/%Y"),
+    AN_BASE > 2016 ~ parse_date_time(DT_TITULACAO, c("%d%b%y:%H:%M:%S"), 
+                                     locale = Sys.setlocale("LC_TIME", "en")))) 
+
+teses_dissertacoes <- teses_dissertacoes_2013_2020 |> 
+  bind_rows(teses_dissertacoes_2010_2012)
+
+
+teses_dissertacoes |> write_rds("dados/bruto/teses_dissertacoes.rds")
+
+# Bolsas de programas de Pós-Graduação -----------------------------------------
 bolsas_programa <- list.files(path = "dados/brutos/bolsas_programas/",
                                  pattern = "*.zip",
                                  full.names = TRUE) |>
@@ -54,4 +104,22 @@ bolsas_programa <- list.files(path = "dados/brutos/bolsas_programas/",
 bolsas_programa |> write_rds("dados/brutos/bolsas_programas.rds")
 
 
-bolsas_programa |> filter(SG_PROGRAMA_CAPES == 'FAPESQ') |> View()
+# Autor da ProduÃ§Ã£o de PeriÃ³dicos
+autor_producao_periodicos <- list.files(path = "dados/brutos/producao_autor/",
+                                 pattern = "*.zip",
+                                 full.names = TRUE) |>
+  map_df(read_csv2,
+         locale = locale(encoding = 'latin5'),
+         col_types = cols(.default = "c"))
+
+autor_producao_periodicos |> write_rds("dados/brutos/autor_producao_periodicos.rds")
+
+# Bolsas de Mobilidade Internacional -----------------------------------------
+bolsas_mobilidade <- list.files(path = "dados/brutos/bolsas_mobilidade_internacional/",
+                                 pattern = "*.zip",
+                                 full.names = TRUE) |>
+  map_df(read_csv2,
+         locale = locale(encoding = 'latin5'),
+         col_types = cols(.default = "c"))
+
+bolsas_mobilidade |> write_rds("dados/brutos/bolsas_mobilidade.rds")
--- R/2.tratar_dados.R
+++ R/2.tratar_dados.R
@@ -1,23 +1,41 @@
 #### Packages -----------
 rm(list = ls())
-needs::needs(tidyverse, vroom)
-
+needs::needs(tidyverse, vroom, lubridate)
 
 #### DADOS -------------
+discentes <- read_rds("dados/bruto/discentes.rds")
+teses_dissertacoes <- read_rds("dados/bruto/teses_dissertacoes.rds")
+bolsas_mobilidade <- read_rds("dados/bruto/bolsas_mobilidade.rds")
 
-discentes <- read_rds("dados/tidy/discentes.rds")
-producao_artigos_periodicos <- read_rds("dados/tidy/producao_artigos_periodicos.rds")
-teses_dissertacoes <- read_rds("dados/tidy/teses_dissertacoes.rds")
+#### DISCENTES PARAÍBA
+discentes_pb <- discentes |> 
+  filter(SG_UF_PROGRAMA == "PB") |> 
+  mutate(across(c(AN_BASE, CD_AREA_AVALIACAO, AN_NASCIMENTO_DISCENTE,
+                  CD_ENTIDADE_CAPES, CD_CONCEITO_PROGRAMA, CD_CONCEITO_CURSO,
+                  ID_PESSOA, QT_MES_TITULACAO), as.numeric)) |> 
+  mutate(across(c(DT_MATRICULA_DISCENTE), parse_date_time, "%d%b%y:%H:%M:%S")) |>
+  mutate(IDADE = AN_BASE - AN_NASCIMENTO_DISCENTE) |>
+  select(AN_BASE, NM_DISCENTE, ID_PESSOA, NR_DOCUMENTO_DISCENTE, 
+         NM_PAIS_NACIONALIDADE_DISCENTE, IDADE, NM_SITUACAO_DISCENTE, 
+         DS_GRAU_ACADEMICO_DISCENTE, DT_MATRICULA_DISCENTE, QT_MES_TITULACAO,
+         SG_ENTIDADE_ENSINO, CD_PROGRAMA_IES, NM_GRANDE_AREA_CONHECIMENTO,
+         CD_ENTIDADE_CAPES, NM_PROGRAMA_IES, NM_MODALIDADE_PROGRAMA, 
+         NM_GRAU_PROGRAMA, NM_MUNICIPIO_PROGRAMA_IES, CD_CONCEITO_PROGRAMA, 
+         CD_CONCEITO_PROGRAMA) |> 
+  mutate(across(where(is.character), ~na_if(., "NÃO SE APLICA"))) 
 
-
-
-discentes_pb <- discentes |> filter(SG_UF_PROGRAMA == "PB")
 discentes_pb |> write_rds("dados/tidy/discentes_pb.rds")
 
+##### TESES E DISSERTAÇÕES PARAÍBA
+
 teses_dissertacoes_pb <- teses_dissertacoes |> filter(SG_UF_IES == "PB")
+
+
 teses_dissertacoes_pb |> write_rds("dados/tidy/teses_dissertacoes_pb.rds")
 
-# 23931 sem tese/dissertação; 25119 com tese/dissertação
-discentes_pb$ID_PESSOA %in% teses_dissertacoes_pb$ID_PESSOA_DISCENTE |> table()
+
+
+# discentes_pb |> skimr::skim()
+
 
 
