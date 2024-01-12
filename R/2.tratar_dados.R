5c49a39ed027e531fcdeaa8a64f1f678bbbaf08b
robson.o.l@hotmail.com <robson.o.l@hotmail.com>
2024-01-12 16:51:31 -0300

    analisar casos edge

--- R/2.tratar_dados.R
+++ R/2.tratar_dados.R
@@ -18,37 +18,6 @@ bolsas_pb <- read_rds("dados/tidy/bolsas_pb.rds")
 # 3. DISCENTES e BOLSAS: unir por NM_DISCENTE e NM_BOLSISTA (??). Talvez área.
 # 4. BOLSAS: Existe ID_PESSOA antes de 2013
 
-# EDGE CASES
-
-# Discentes com mais de uma ocorrência em um mesmo ano.
-# contagem = 2 pode indicar que alguém defendeu no ano X e cursou doutorado no mesmo ano.
-# mas contagem > 2? (215 observações)
-
-discentes_pb |> group_by(NM_DISCENTE, ANO) |> 
-  mutate(contagem = n()) |> group_by(contagem) |> count()
-
-# contagem        n
-#        1   124570
-#        2     7480
-#        3      207
-#        4        8
-
-# Jualiana aparece em 4 programas diferentes da UFCG em 2004; no ano de 2005, em 3;
-# em 2006 e 2007, em 2; e no ano de 2008, em 1 programa. Em nenhum como concluído.
-discentes_pb |> 
-  filter(NM_DISCENTE == 'JULIANA DE CASTRO MACEDO FONSECA') |> 
-  left_join(bolsas_pb) |> View()
-
-
-discentes_pb |> 
-  group_by(NM_DISCENTE, ANO) |> 
-  mutate(contagem = n()) |> 
-  ungroup() |> 
-  filter(contagem > 2) |> 
-  relocate(contagem, .before = everything()) |> 
-  arrange(desc(contagem)) |> 
-  View()
-
 
   
 discentes_pb |> 
@@ -65,6 +34,3 @@ discentes_pb |>
   relocate(NR_DOCUMENTO_DISCENTE, .after = NR_DOCUMENTO) |> 
   relocate(ID_PESSOA.x, .after = ID_PESSOA.y) |>
   View()
-
-
-
