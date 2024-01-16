8c3f2084be49ff804442de847c7773d76f011630
robson.o.l@hotmail.com <robson.o.l@hotmail.com>
2024-01-16 15:56:26 -0300

    df base

--- R/2.tratar_dados.R
+++ R/2.tratar_dados.R
@@ -9,6 +9,7 @@ library(lubridate)
 discentes_pb <- read_rds("dados/tidy/discentes_pb.rds")
 teses_pb <- read_rds("dados/tidy/teses_dissertacoes_pb.rds")
 bolsas_pb <- read_rds("dados/tidy/bolsas_pb.rds")
+artigos_autor_pb <- read_rds("dados/tidy/artigos_autor_pb.rds")
 
 #### DISCENTES PARAÍBA
 
@@ -18,19 +19,30 @@ bolsas_pb <- read_rds("dados/tidy/bolsas_pb.rds")
 # 3. DISCENTES e BOLSAS: unir por NM_DISCENTE e NM_BOLSISTA (??). Talvez área.
 # 4. BOLSAS: Existe ID_PESSOA antes de 2013
 
+df <- discentes_pb |> 
+  filter(ANO >= 2013) |> 
+  distinct(ID_PESSOA, NM_DISCENTE, AN_NASCIMENTO_DISCENTE, NR_DOCUMENTO_DISCENTE) |> 
+  crossing(ANO = seq(2013, 2020)) |> 
+  relocate(ANO, .before = ID_PESSOA) |>
+  mutate(IDADE_DISCENTE = ANO - AN_NASCIMENTO_DISCENTE) |> 
+  # Algumas pessoas tem nomes diferentes ao longo dos anos:
+  # ID_PESSOA == 51331 se chama ANNE KARINE DE SOUZA NASCIMENTO
+  # e em outros momentos se chama ANNE KARINE DE SOUZA NASCIMENTO SOARES
+  distinct(ID_PESSOA, ANO, .keep_all = TRUE)
 
-  
-discentes_pb |> 
-  select(ANO, NM_DISCENTE, NR_DOCUMENTO_DISCENTE, ID_PESSOA, IDADE, NM_SITUACAO_DISCENTE,
-         SG_ENTIDADE_ENSINO, NM_PROGRAMA_IES, DS_GRAU_ACADEMICO_DISCENTE,
-         NM_ORIENTADOR) |> 
-  #group_by(ANO, NM_DISCENTE) |>
-  #mutate(contagem = n()) |> 
-  #ungroup() |> 
-  left_join(bolsas_pb |> select(ANO, NM_DISCENTE, ID_PESSOA, NR_DOCUMENTO,
-                                SG_IES_ESTUDO, DS_NIVEL, NM_PROGRAMA_PPG,
-                                QT_BOLSA_ANO, VL_BOLSA_ANO),
-            by = c("ANO", "NM_DISCENTE")) |> 
-  relocate(NR_DOCUMENTO_DISCENTE, .after = NR_DOCUMENTO) |> 
-  relocate(ID_PESSOA.x, .after = ID_PESSOA.y) |>
-  View()
+# df |> group_by(ID_PESSOA) |>summarise(contagem = n()) |> 
+#   group_by(contagem) |> summarise(n = n()) # todo mundo tem 8 anos de informações
+
+
+# É BOLSISTA/ANO/PESSOA
+df <- df |> left_join(
+  bolsas_pb |> 
+    select(ID_PESSOA, ANO, QT_BOLSA_ANO, VL_BOLSA_ANO, DS_NIVEL),
+  by = c("ID_PESSOA", "ANO")
+) |> 
+  mutate(BOLSISTA = ifelse(
+    !is.na(VL_BOLSA_ANO), 1, 0
+  )) |> 
+  relocate(BOLSISTA, .after = IDADE_DISCENTE)
+
+
