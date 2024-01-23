f1d8da3d626a45bfca6d12d68d1adc6bbe57307e
aviniciusbb <aviniciusbb@hotmail.com>
2024-01-23 18:25:08 -0300

    update_indicadores

--- R/3.indicadores.R
+++ R/3.indicadores.R
@@ -5,5 +5,23 @@ rm(list = ls())
 pacman::p_load(tidyverse, janitor)
 
 # DADOS ------
-
 discentes_bolsa_tese_pub <- readr::read_rds("dados/tidy/discentes_bolsa_tese_pub.rds")
+
+
+# Bolsistas Fapesq -----
+
+bolsista_fapesq <- discentes_bolsa_tese_pub |> 
+  dplyr::filter(
+    BOLSA_APENAS_FAPESQ == 1
+  )
+  
+out <- bolsista_fapesq |> 
+  dplyr::group_by(
+    DS_GRAU_ACADEMICO_DISCENTE
+  ) |> 
+  dplyr::summarise(
+    TX_CONCLUSAO_FAPESQ = sum(TEM_TESE)/sum(BOLSA_APENAS_FAPESQ) * 100
+  )
+
+
+  
