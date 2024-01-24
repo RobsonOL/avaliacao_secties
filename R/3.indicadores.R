cfaea000e62ca6cde836109ed390b48a4e3338e6
aviniciusbb <aviniciusbb@hotmail.com>
2024-01-24 09:37:44 -0300

    update_indicadores

--- R/3.indicadores.R
+++ R/3.indicadores.R
@@ -14,8 +14,34 @@ bolsista_fapesq <- discentes_bolsa_tese_pub |>
   dplyr::filter(
     BOLSA_APENAS_FAPESQ == 1
   )
+
+# Numero de bolsistas
+n_bolsistas <- bolsista_fapesq |> 
+  dplyr::group_by(
+    DS_GRAU_ACADEMICO_DISCENTE
+  ) |>
+  dplyr::summarise(
+    n_bolsistas = n()
+  )
+
+# Numero de bolsistas por genero
+n_bolsistas_genero <- bolsista_fapesq |> 
+  dplyr::group_by(
+    DS_GRAU_ACADEMICO_DISCENTE, GENERO
+  ) |>
+  dplyr::summarise(
+    n_bolsistas_genero = n()
+  ) |> 
+  dplyr::ungroup() |> 
+  dplyr::group_by(
+    DS_GRAU_ACADEMICO_DISCENTE
+  ) |> 
+  dplyr::mutate(
+    freq = n_bolsistas_genero/sum(n_bolsistas_genero) * 100
+  )
   
-out <- bolsista_fapesq |> 
+# Taxa de titulacao
+tx_conclusao <- bolsista_fapesq |> 
   dplyr::group_by(
     DS_GRAU_ACADEMICO_DISCENTE
   ) |> 
@@ -23,5 +49,23 @@ out <- bolsista_fapesq |>
     TX_CONCLUSAO_FAPESQ = sum(TEM_TESE)/sum(BOLSA_APENAS_FAPESQ) * 100
   )
 
+# Numero medio de bolsas
+n_medio_bolsas <- bolsista_fapesq |> 
+  dplyr::group_by(
+    DS_GRAU_ACADEMICO_DISCENTE
+  ) |>
+  dplyr::summarise(
+    n_medio_bolsas = mean(QT_BOLSA_TOTAL)
+  )
 
-  
+# Valor medio de bolsas
+valor_medio_bolsas <- bolsista_fapesq |> 
+  dplyr::group_by(
+    DS_GRAU_ACADEMICO_DISCENTE
+  ) |>
+  dplyr::summarise(
+    valor_medio_bolsas = mean(VL_BOLSA_TOTAL)
+  )
+
+ 
+
