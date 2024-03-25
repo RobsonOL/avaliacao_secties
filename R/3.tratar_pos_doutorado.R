02617b56c8c918ac5805c6eae80957c34cb0fa9d
Robson Oliveira <robson.lima@ifpb.edu.br>
2024-03-25 13:10:32 -0300

    adiciona producao a posdoc fapesq

--- R/3.tratar_pos_doutorado.R
+++ R/3.tratar_pos_doutorado.R
@@ -3,26 +3,72 @@ rm(list = ls())
 pacman::p_load(tidyverse)
 
 # DADOS ------
+artigos_autor_pb <- readr::read_rds("dados/tidy/artigos_autor_pb.rds") |> 
+  dplyr::filter(
+    !is.na(SG_ESTRATO), SG_ESTRATO != "NI"
+  ) |>
+  dplyr::group_by(
+    NM_AUTOR, SG_ESTRATO
+  ) |>
+  dplyr::summarise(
+    ARTIGOS = n()
+  ) |> 
+  dplyr::ungroup() |> 
+  tidyr::pivot_wider(
+    id_cols = NM_AUTOR, 
+    names_from = SG_ESTRATO, 
+    values_from = ARTIGOS, 
+    names_prefix = "ARTIGO_", values_fill = 0
+  ) |> 
+  dplyr::rowwise() |> 
+  dplyr::mutate(
+    TOTAL_ARTIGOS = sum(c_across(starts_with('ARTIGO_')), na.rm = TRUE)
+  ) 
+
+
 fapesq <- readr::read_rds("dados/tidy/editais_fapesq.rds")|> 
-  dplyr::filter(DS_GRAU_ACADEMICO_DISCENTE == "POS-DOUTORADO")
+  dplyr::mutate(TIPO_BOLSA = "FAPESQ") |>
+  dplyr::select(-TIPO_BOLSA_MAIS_COMUM) |> 
+  dplyr::filter(DS_GRAU_ACADEMICO_DISCENTE == "POS-DOUTORADO") |> 
+  dplyr::mutate(ANO = lubridate::year(INICIO_BOLSA)) |> 
+  dplyr::mutate(IDADE = ANO - lubridate::year(DT_NASCIMENTO_DISCENTE))
+
+fapesq_artigos <- fapesq |> 
+  left_join(artigos_autor_pb, by = c("NM_DISCENTE" = "NM_AUTOR"))
 
 
-fapesq |> readr::write_rds("dados/tidy/fapesq_pos_doutorado.rds")
 
+fapesq_artigos|> readr::write_rds("dados/tidy/fapesq_pos_doutorado.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# DEMAIS BOLSAS ----------
 cnpq <- read_rds("dados/tidy/bolsas_cnpq_pb.rds") |>
   dplyr::filter(DS_GRAU_ACADEMICO_DISCENTE == "POS-DOUTORADO") |>
+  dplyr::mutate(TIPO_BOLSA = "CNPQ") |> 
   dplyr::group_by(NM_DISCENTE,
                   ANO,
                   TIPO_BOLSA,
                   DS_GRAU_ACADEMICO_DISCENTE,
                   SG_ENTIDADE_ENSINO) |>
   dplyr::summarise(VL_BOLSA_ANO = sum(VL_BOLSA_ANO, na.rm = TRUE)) |>
-  dplyr::ungroup() |>
-  dplyr::rename(VL_BOLSA_CNPQ = VL_BOLSA_ANO)
+  dplyr::ungroup()
 
 cnpq |> readr::write_rds("dados/tidy/cnpq_pos_doutorado.rds")
 
 capes <- read_rds("dados/tidy/bolsas_pb.rds") |> 
+  dplyr::mutate(TIPO_BOLSA = "CAPES") |>
   dplyr::select(-c(DS_PROJETO, CONCEITO_ANO,
                    contains(
                      c("ORIGEM", "TAXA", "MULTICAMPI", "STATUS")
@@ -51,7 +97,19 @@ capes <- read_rds("dados/tidy/bolsas_pb.rds") |>
   dplyr::group_by(ID_PESSOA, ANO, DS_NIVEL, CD_PROGRAMA_PPG, SG_ENTIDADE_ENSINO, SG_PROGRAMA_CAPES) |>
   dplyr::mutate(QT_BOLSA_ANO = sum(QT_BOLSA_ANO),
                 VL_BOLSA_ANO = sum(VL_BOLSA_ANO)) |> 
+  dplyr::ungroup() |> 
   dplyr::distinct(.keep_all = TRUE) 
 
 
 capes |> readr::write_rds("dados/tidy/capes_pos_doutorado.rds")
+
+
+
+# Unir as bases ----
+df <- fapesq |> 
+  dplyr::bind_rows(cnpq) |>
+  dplyr::bind_rows(capes)
+
+
+df |> 
+  group_by(TIPO_BOLSA) |> slice(1) |> View()
